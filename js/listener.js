92184c2b043c2ff1e23d917aa1f4c476a2afca48
mangoeis <60978002+CaptainMango1@users.noreply.github.com>
2022-11-11 11:32:52 +0100

    Finished animation

--- js/listener.js
+++ js/listener.js
@@ -3,10 +3,20 @@ const UG = 0;
 const input = document.getElementById('input');
 const output = document.getElementById('out');
 const submit = document.getElementById('submit');
+const img = document.getElementById('ball');
 
 submit.addEventListener('click', () => {
     let ans = ['Yes', 'No', 'Maybe', '...', 'I am not sure'];
     let ansNum = Math.round(Math.random() * OG);
 
-    output.innerHTML = ans[ansNum];
+    img.classList.add('animate-ball');
+
+    output.innerHTML = 'Thinking ...';
+
+    setTimeout(function () {
+
+        output.innerHTML = ans[ansNum];
+        img.classList.remove('animate-ball');
+    }, 2000);
+
 });
